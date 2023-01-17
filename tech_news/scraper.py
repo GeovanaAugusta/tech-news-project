a6d7047cce5a0d06892cb1746885b46d752a2361
Geovana Augusta <geovanaag@hotmail.com>
2023-01-17 19:26:53 -0300

    feat: def that fetch a HTTP request

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,12 +1,35 @@
+from time import sleep
+import requests
+from parsel import Selector
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
-    # initial commit
+    try:
+        # Garante que cada req seja feita com um intervalo de 1 s, antes da req
+        sleep(1)
+        # requisição para essa instância criada utilizando o método `get`
+        response = requests.get(url,
+                                headers={"user-agent": "Fake user-agent",
+                                         "Accept": "text/html"}, timeout=3)
+        # print(response)
+        get_status_code = response.status_code
+
+# use ==/!= to compare constant literals (str, bytes, int, float, tuple),
+# quando tentei usar is
+        if get_status_code == 200:
+            return response.text
+        else:
+            return None
+
+    except requests.exceptions.Timeout:
+        return None
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    print(selector)
 
 
 # Requisito 3
@@ -22,3 +45,25 @@ def scrape_news(html_content):
 # Requisito 5
 def get_tech_news(amount):
     """Seu código deve vir aqui"""
+
+# SOURCE
+# Dia 02 - Exercício 3
+# response = requests.get(
+#     "https://scrapethissite.com/pages/advanced/?gotcha=headers",
+#     headers={"User-agent": "Mozilla", "Accept": "text/html"},
+# )
+# https://www.datacamp.com/tutorial/making-http-requests-in-python
+# Estrutura base
+#     try:
+
+    # Busca pela tag 'a' com as classes específicas do link desejado
+    #     return soup_page.find(
+    #         "a",
+    #         {"class": "tec--btn"},
+    #     )["href"]
+    # except TypeError:
+    #     return None
+# Pegar apenas o número do status retornado
+# https://www.pluralsight.com/guides/web-scraping-with-request-python
+# Except para quando extrapola o timeout
+# https://stackoverflow.com/questions/28377421/why-do-i-receive-a-timeout-error-from-pythons-requests-module
