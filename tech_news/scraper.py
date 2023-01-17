7ee63b82e07b3125adcc9c055d39e00ff885943e
Geovana Augusta <geovanaag@hotmail.com>
2023-01-17 19:42:47 -0300

    feat: def that get urls for news in a page news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,6 +31,13 @@ def scrape_updates(html_content):
     selector = Selector(text=html_content)
     print(selector)
 
+    news = selector.css(".entry-title a::attr(href)").getall()
+
+    if len(news) > 0:
+        return news
+    else:
+        return []
+
 
 # Requisito 3
 def scrape_next_page_link(html_content):
@@ -47,6 +54,8 @@ def get_tech_news(amount):
     """Seu código deve vir aqui"""
 
 # SOURCE
+
+# Requisito 1
 # Dia 02 - Exercício 3
 # response = requests.get(
 #     "https://scrapethissite.com/pages/advanced/?gotcha=headers",
@@ -67,3 +76,15 @@ def get_tech_news(amount):
 # https://www.pluralsight.com/guides/web-scraping-with-request-python
 # Except para quando extrapola o timeout
 # https://stackoverflow.com/questions/28377421/why-do-i-receive-a-timeout-error-from-pythons-requests-module
+
+# Requisito 2
+# Dia 02
+# response = requests.get("http://books.toscrape.com/")
+# selector = Selector(text=response.text)
+# print(selector)
+
+# O título está no atributo title em um elemento âncora (<a>)
+# Dentro de um h3 em elementos que possuem classe product_pod
+# titles = selector.css(".product_pod h3 a::attr(title)").getall()
+# Tamanho de um array 
+# https://www.digitalocean.com/community/tutorials/find-the-length-of-a-list-in-python
